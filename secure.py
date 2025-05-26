c33489660b0f5db49c19d069c6edc3449d8a59fc
Mohamed Mohy <105385056+MohamedMohy0@users.noreply.github.com>
2025-05-26 18:09:34 +0300

    Update secure.py

--- secure.py
+++ secure.py
@@ -1,12 +1,16 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-st.markdown("""
-    <script>
-    document.addEventListener('contextmenu', function(event) {
-        event.preventDefault();
-        alert("Right-click is disabled inside this app.");
-    });
-    </script>
-    """, unsafe_allow_html=True)
+st.title("Disable right-click inside app area")
 
-st.write("Try right-clicking inside the app area now.")
+js = """
+<script>
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+    alert("Right-click is disabled inside this app.");
+});
+</script>
+"""
+
+components.html(js)
+st.write("Try right-clicking anywhere inside this app area.")
